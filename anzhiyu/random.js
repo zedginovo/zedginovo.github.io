82768cdbaef927829f17fddf16b93837330651e0
1 <zedgin@126.com>
2024-02-11 22:09:42 +0800

    Site updated: 2024-02-11 22:09:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/11/hello-world/"];function toRandomPost(){
+var posts=["2024/02/11/111/","2024/02/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
